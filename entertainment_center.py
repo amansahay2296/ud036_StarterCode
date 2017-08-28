c933dd89d790ff04941050de72cb24d26fbf2fb4
Aman Sahay <amansahay2296@gmail.com>
2017-08-28 19:12:42 +0530

    Changes after Project Review

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,23 +1,42 @@
 import media
 import fresh_tomatoes
 
-_3idiots=media.Movie("3 Idiots","https://upload.wikimedia.org/wikipedia/en/d/df/3_idiots_poster.jpg","https://www.youtube.com/watch?v=K0eDlFX9GMc")
+#Creating instances for class Movie
 
-bourne_identity=media.Movie("The Bourne Identity","https://upload.wikimedia.org/wikipedia/en/a/ae/BourneIdentityfilm.jpg","https://www.youtube.com/watch?v=FpKaB5dvQ4g")
+_3idiots=media.Movie("3 Idiots","https://upload.wikimedia.org/wikipedia/en/d/df/"+
+                     "3_idiots_poster.jpg",
+                     "https://www.youtube.com/watch?v=K0eDlFX9GMc")
 
-
-a_few_good_men=media.Movie("A Few Good Men","https://upload.wikimedia.org/wikipedia/en/4/45/A_Few_Good_Men_poster.jpg","https://www.youtube.com/watch?v=ePo91pMcu94")
-
-batman_begins=media.Movie("Batman Begins","https://upload.wikimedia.org/wikipedia/en/a/af/Batman_Begins_Poster.jpg","https://www.youtube.com/watch?v=QhPqez3CwiM")
-
-prestige=media.Movie("The Prestige","https://upload.wikimedia.org/wikipedia/en/d/d2/Prestige_poster.jpg","https://www.youtube.com/watch?v=o4gHCmTQDVI")
-
-
-rdb=media.Movie("Rang De Basanti","https://upload.wikimedia.org/wikipedia/en/5/5f/RDB_poster.jpg","https://www.youtube.com/watch?v=l-BTOTtcGmk")
+bourne_identity=media.Movie("The Bourne Identity","https://upload.wikimedia.org/"+
+                            "wikipedia/en/a/ae/BourneIdentityfilm.jpg",
+                            "https://www.youtube.com/watch?v=FpKaB5dvQ4g")
 
 
+a_few_good_men=media.Movie("A Few Good Men","https://upload.wikimedia.org/"+
+                           "wikipedia/en/4/45/A_Few_Good_Men_poster.jpg",
+                           "https://www.youtube.com/watch?v=ePo91pMcu94")
+
+batman_begins=media.Movie("Batman Begins","https://upload.wikimedia.org/"+
+                          "wikipedia/en/a/af/Batman_Begins_Poster.jpg",
+                          "https://www.youtube.com/watch?v=QhPqez3CwiM")
+
+prestige=media.Movie("The Prestige","https://upload.wikimedia.org/wikipedia/"+
+                     "en/d/d2/Prestige_poster.jpg",
+                     "https://www.youtube.com/watch?v=o4gHCmTQDVI")
+
+
+rdb=media.Movie("Rang De Basanti","https://upload.wikimedia.org/wikipedia/"+
+                "en/5/5f/RDB_poster.jpg",
+                "https://www.youtube.com/watch?v=l-BTOTtcGmk")
+
+# Storing all Movie instances in a list 
 movies =[_3idiots,bourne_identity,a_few_good_men,batman_begins,prestige,rdb]
 
+
+
+#function call
+#open_movies_page(movies) takes a list of Movie objects as an argument
+#and generates the movie trailer website.
 fresh_tomatoes.open_movies_page(movies)
 
 
--- media.py
+++ media.py
@@ -1,8 +1,10 @@
 
 class Movie():
-    """This class heps us to store movie related information"""
+    """This class helps us to store movie related information"""
     
     def __init__(self,movie_title,poster_image,trailer_youtube):
+        """  Constructor call for class Movie with instance variables
+             title,poster_image_url,trailer_youtube_url"""
         self.title=movie_title
         self.poster_image_url=poster_image
         self.trailer_youtube_url=trailer_youtube
