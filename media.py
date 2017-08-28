bd24c81578a04253b0e78b7ca50b921001524085
Aman Sahay <amansahay2296@gmail.com>
2017-08-28 21:18:43 +0530

    After Styling

--- media.py
+++ media.py
@@ -1,13 +1,12 @@
-
 class Movie():
-    """This class helps us to store movie related information"""
-    
-    def __init__(self,movie_title,poster_image,trailer_youtube):
+    """This class helps us to store movie related information """
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         """  Constructor call for class Movie with instance variables
              title,poster_image_url,trailer_youtube_url"""
-        self.title=movie_title
-        self.poster_image_url=poster_image
-        self.trailer_youtube_url=trailer_youtube
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
         
     
         
